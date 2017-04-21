9f048987e700eb8739cf590abb0b4b4a7b5f31f2
ShogoNogi <s8n10ax@gmail.com>
2017-04-21 17:29:34 +0900

    全体的に修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
     :registrations => 'users/registrations'
   }
   resources :users, only: [:show, :edit, :update], shallow: true do
-    resources :lessons, only: [:index, :destroy, :show]
+    resources :lessons, only: [:index], shallow: true do
+      resources :sentence_statuses, only: [:show]
+      post 'sentence_statues/:id/ttoggle' => 'sentence_statuses#ttoggle'
+      post 'sentence_statues/:id/ftoggle' => 'sentence_statuses#ftoggle'
+    end
     post 'lessons/:id/toggle' => 'lessons#toggle'
   end
   resources :sentence_data, only: [:show]
